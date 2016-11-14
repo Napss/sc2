73de266bdf350505b0d12d2ed902e438530a432f
Napss <napss@ymail.com>
2016-11-14 10:00:46 +0700

    Update client.c

--- test/client.c
+++ test/client.c
@@ -1,105 +1,89 @@
-#include <math.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
+
+#include "include/sockutil.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "include/sockutil.h"
 
-void sendfile(int sockfd,char *path);
-void listfiles(int sockfd,char *path);
-char * getchoice(int cli_sockfd);
+void rcvfile(int sockfd, char *outputname);
+void rcvlist(int sockfd);
+void choosefile(int sockfd);
+char * namefile();
 
 int main(int argc, char** argv)
 {
-	if (argc < 2) {
-		puts("Missing port");
+
+	char * outputname;
+	if (argc < 3) {
+		puts("Missing host and port");
 		exit(0);
 	}
 
-	int sockfd = tcp_sock_serv(atoi(argv[1]));
-
-	puts("Server is listening to incoming connections");
-	int cli_sockfd, pid;
-
-	/* Support multiple connections  */
-	char * path;
-
-	while (1) {
-		cli_sockfd = tcp_sock_accept(sockfd);
-		pid = fork();
-		if (pid < 0)
-			error("Can not fork");
-		else if (pid == 0){		
-
-			listfiles(cli_sockfd,".");
-			path = getchoice(cli_sockfd);
-			printf("%s\n", path);
-			sendfile(cli_sockfd,path);
-			puts("Closing client's socket");
-			close(cli_sockfd);
-		} else {
-			printf("Spawned a process for new connection: %d\n", pid);
-			continue;
-		}
-	}
-
-	puts("Closing server's socket");
+	int sockfd = tcp_sock_init();
+	tcp_sock_connect(sockfd, argv[1], atoi(argv[2]));
+	rcvlist(sockfd);
+	choosefile(sockfd);
+	outputname = namefile();
+	rcvfile(sockfd,outputname);
 	close(sockfd);
 }
 
 
-void sendfile(int sockfd,char *file_path){
+void rcvfile(int sockfd,char *outputname){
 	char buffer[BUFSIZ];
 	int filefd;
+	char *file_path;
 	ssize_t read_return;
-    filefd = open(file_path, O_RDWR);
-	while (1) {
-		read_return = read(filefd, buffer, BUFSIZ);
-		//puts("send");
-		if (read_return == 0)
-			break;
+    filefd = open(outputname,O_WRONLY | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR);
+	if (filefd == -1) {
+		perror("open");
+		exit(EXIT_FAILURE);
+	}
+
+	do {
+		read_return = read(sockfd, buffer, BUFSIZ);
 		if (read_return == -1) {
 			perror("read");
 			exit(EXIT_FAILURE);
 		}
-		if (write(sockfd, buffer, read_return) == -1) {
+		if (write(filefd, buffer, read_return) == -1) {
 			perror("write");
 			exit(EXIT_FAILURE);
 		}
-	}
-	puts("Finish");
+		}
+		while (read_return==8192);
+
+		
+	close(filefd);
+	close(sockfd);
+		
+
+	}	
+
+void rcvlist(int sockfd){
+	char *list=" ";
+	char buffer[BUFSIZ];
+	int bytes = recv(sockfd, buffer, sizeof(buffer), 0);
+	buffer[bytes] = '\0';
+	printf("%s", buffer);
+
 }
 
-void listfiles(int sockfd,char *file_path){
+void choosefile(int sockfd){
+
 	char buffer[BUFSIZ];
-    DIR *d;
-    struct dirent *dir;
-    char *list = "Fichiers  : ";
-	strcpy(buffer, list);
-    d = opendir(file_path);
-    if (d){
-        while ((dir = readdir(d)) != NULL){
-           strcat(buffer,dir->d_name);
-           strcat(buffer," ");
-
-        }
-       	strcat(buffer,"\n");
-
-        closedir(d);
-    }
+	puts("Choose file : ");
+ 	fgets(buffer, sizeof(buffer), stdin);
     send(sockfd, buffer, strlen(buffer), 0);
 }
-
-char * getchoice(int cli_sockfd){
+char * namefile(){
 	char buffer[BUFSIZ];
-	char *choice;
-	int bytes = recv(cli_sockfd, buffer, sizeof(buffer), 0);
-	printf("%i\n", bytes);
-	buffer[bytes-1] = '\0';
+	char *outputpath;
+	puts("Choose name of copied file : ");
+ 	fgets(buffer, sizeof(buffer), stdin);
 	char* p = &buffer[0] ;
 	return p ;
 }
