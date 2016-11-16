0d44f72a45af4fb0d6fa5021f7718972d3c02eb6
Napss <napss@ymail.com>
2016-11-16 17:53:10 +0700

    Update client.c

--- test/client.c
+++ test/client.c
@@ -1,31 +1,25 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "include/sockutil.h"
+#include "include/filetransfer.h"
+
+
 
 int main(int argc, char** argv)
 {
-	if (argc < 3) {
+	if (argc < 3)
+	{
 		puts("Missing host and port");
 		exit(0);
 	}
-
 	int sockfd = tcp_sock_init();
-
 	tcp_sock_connect(sockfd, argv[1], atoi(argv[2]));
-
-	char buffer[256];
-	puts("Connection is ready, put two integers below");
-	fgets(buffer, 256, stdin);
-
-	if (write(sockfd, buffer, strlen(buffer)) < 0)
-		error("Failed to write data");
-
-	memset(buffer, '\0', 256);
-	if (read(sockfd, buffer, 256) < 0)
-		error("Failed to get results");
-	puts(buffer);
-
+	download_client(sockfd);
 	close(sockfd);
 }
+
